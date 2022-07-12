2d3119ab5e79277f5dd71b89f4890f828e737473
tian <tianwu@outlook.com>
2022-07-12 17:33:31 +1000

    finish card2

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'author_card.dart';
+import 'fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
@@ -22,7 +24,37 @@ class Card2 extends StatelessWidget {
         child: Column(
           children: [
             // TODO 1: add author info
+            const AuthorCard(
+              authorName: 'Mike Smith',
+              title: 'Smoothie Connoisseur',
+              imageProvider: AssetImage('assets/author_katz.jpeg'),
+            ),
             // TODO 4: add Positioned text
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom: 8,
+                    right: 8,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  ),
+                  Positioned(
+                    left: 8,
+                    bottom: 50,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
