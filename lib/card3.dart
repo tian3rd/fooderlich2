1d82514cc1c3e391334d7ca2056ced92cb094219
tian <tianwu@outlook.com>
2022-07-12 17:44:30 +1000

    add basic card placeholder

---
+++ lib/card3.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+
+class Card3 extends StatelessWidget {
+  const Card3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Stack(
+          children: [
+            // TODO 5: add dark overlay BoxDecoration
+            // TODO 6: add Container, Column, Icon and Text
+            // TODO 7: add Center widget with Chip widget children
+          ],
+        ),
+        padding: const EdgeInsets.all(16),
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag2.png'),
+            fit: BoxFit.cover,
+          )
+        )
+      ),
+    );
+  }
+}
