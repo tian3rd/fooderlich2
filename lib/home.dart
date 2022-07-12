49a7c22c5d742f971d41f7651624f0e3ebd4d977
tian <tianwu@outlook.com>
2022-07-12 10:39:14 +1000

    extract home widget

---
+++ lib/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // TODO: add state variables and functions
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fooderlich App',
+          style: Theme.of(context).textTheme.headline6,),
+      ),
+      body: Center(
+        child: Text('Let\'s get cooking 👩‍🍳',
+          style: Theme.of(context).textTheme.headline1,),
+      ),
+    );
+  }
+}
