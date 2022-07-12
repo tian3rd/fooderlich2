ca80c2ec97fb345aee0163ee0a847df1027d263f
tian <tianwu@outlook.com>
2022-07-12 14:54:56 +1000

    add text stack to recipe card

--- lib/card1.dart
+++ lib/card1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 class Card1 extends StatelessWidget {
   const Card1({Key? key}) : super(key: key);
@@ -13,6 +14,37 @@ class Card1 extends StatelessWidget {
       // TODO: Card1 decorate container
       child: Container(
         // TODO: add a stack of text
+        child: Stack(
+          children: [
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline2,
+              ),
+              top: 20,
+            ),
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            ),
+          ],
+        ),
         padding: const EdgeInsets.all(16),
         constraints: const BoxConstraints.expand(
           width: 350,
