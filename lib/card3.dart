c6043c8059e780a169d6f30e6619d879fb76b40d
tian <tianwu@outlook.com>
2022-07-12 17:50:28 +1000

    add overlay

--- lib/card3.dart
+++ lib/card3.dart
@@ -11,11 +11,17 @@ class Card3 extends StatelessWidget {
         child: Stack(
           children: [
             // TODO 5: add dark overlay BoxDecoration
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.6),
+                borderRadius: const BorderRadius.all(Radius.circular(16)),
+              ),
+            ),
             // TODO 6: add Container, Column, Icon and Text
             // TODO 7: add Center widget with Chip widget children
           ],
         ),
-        padding: const EdgeInsets.all(16),
+        // no need for padding here because the overlay can handle it
         constraints: const BoxConstraints.expand(
           width: 350,
           height: 450,
@@ -24,7 +30,8 @@ class Card3 extends StatelessWidget {
           image: DecorationImage(
             image: AssetImage('assets/mag2.png'),
             fit: BoxFit.cover,
-          )
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(16)),
         )
       ),
     );
