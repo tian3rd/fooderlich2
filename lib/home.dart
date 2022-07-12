0afa6c9e2c63dabc11131d24fbc3438e74b12865
tian <tianwu@outlook.com>
2022-07-12 11:24:43 +1000

    nav between items

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,22 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   // TODO: add state variables and functions
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget> [
+    // TODO: replace with card1
+    Container(color: Colors.red),
+    // TODO: replace with card2
+    Container(color: Colors.green),
+    // TODO: replace with card3
+    Container(color: Colors.blue),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +33,14 @@ class _HomeState extends State<Home> {
         title: Text('Fooderlich App',
           style: Theme.of(context).textTheme.headline6,),
       ),
-      body: Center(
-        child: Text('Let\'s get cooking 👩‍🍳',
-          style: Theme.of(context).textTheme.headline1,),
-      ),
+      // TODO: show selected tab
+      body: pages[_selectedIndex],
       // TODO: add bottom navigation bar
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        // TODO: set selected tab bar
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped, // how does it pass in the index?
         items: <BottomNavigationBarItem> [
           const BottomNavigationBarItem(
             icon: Icon(Icons.card_giftcard),
