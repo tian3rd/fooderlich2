c4fc4a265e224454b52764c5a7bde2616a29b15b
tian <tianwu@outlook.com>
2022-07-16 20:12:37 +1000

    add ListView to holder both today recipe & friend post

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -13,19 +13,31 @@ class ExploreScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO 1: add TodayRecipeListView FutureBuilder
     return FutureBuilder(
-        future: mockService.getExploreData(),
-        builder: (context, AsyncSnapshot<ExploreData> snapshot) {
-          // TODO: add nested list views
-          if (snapshot.connectionState == ConnectionState.done) {
-            final recipes = snapshot.data?.todayRecipes ?? [];
-            // TODO: replace this with TodayRecipeListView
-            return TodayRecipeListView(recipes: recipes);
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        // TODO: add nested list views
+        if (snapshot.connectionState == ConnectionState.done) {
+          // final recipes = snapshot.data?.todayRecipes ?? [];
+          // // TODO: replace this with TodayRecipeListView
+          // return TodayRecipeListView(recipes: recipes);
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              const SizedBox(height: 16),
+              // TODO: replace with FriendPostListView
+              Container(
+                height: 400,
+                color: Colors.grey,
+              ),
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
