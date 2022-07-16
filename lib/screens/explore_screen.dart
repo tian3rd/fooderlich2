aa222713d01389425d4e56815db3c46bae205ebf
tian <tianwu@outlook.com>
2022-07-16 19:32:33 +1000

    add today recipe list view

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -19,11 +19,7 @@ class ExploreScreen extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.done) {
             final recipes = snapshot.data?.todayRecipes ?? [];
             // TODO: replace this with TodayRecipeListView
-            return Center(
-              child: Container(
-                child: const Text('Show TodayRecipeListView'),
-              ),
-            );
+            return TodayRecipeListView(recipes: recipes);
           } else {
             return const Center(
               child: CircularProgressIndicator(),
