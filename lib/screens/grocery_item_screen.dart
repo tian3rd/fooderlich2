4778056ff0ce05545cc5aea6e4d4cba7caab767d
tian <tianwu@outlook.com>
2022-07-18 17:41:40 +1000

    add grocery item state properties

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -17,7 +17,8 @@ class GroceryItemScreen extends StatefulWidget {
     required this.onCreate,
     required this.onUpdate,
     this.originalItem,
-}) : isUpdating = originalItem != null, super(key: key);
+  })  : isUpdating = originalItem != null,
+        super(key: key);
 
   @override
   _GroceryItemScreenState createState() => _GroceryItemScreenState();
@@ -25,14 +26,54 @@ class GroceryItemScreen extends StatefulWidget {
 
 class _GroceryItemScreenState extends State<GroceryItemScreen> {
   // TODO: add grocery item screen state properties
+  final _nameController = TextEditingController();
+  String _name = '';
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+  int _currentSliderValue = 0;
+
+  // TODO: add initState()
+  @override
+  void initState() {
+    final originalItem = widget.originalItem;
+    if (originalItem != null) {
+      _nameController.text = originalItem.name;
+      _name = originalItem.name;
+      _importance = originalItem.importance;
+      _currentColor = originalItem.color;
+      final date = originalItem.date; // why is this final?
+      _timeOfDay = TimeOfDay(hour: date.hour, minute: date.minute);
+      _dueDate = date;
+      _currentSliderValue = originalItem.quantity;
+    }
+
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
+
+    super.initState();
+  }
+
+  // TODO: add dispose()
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO 12: add GroceryItemScreen scaffold
-    return Container(color: Colors.orange,);
+    return Container(
+      color: Colors.orange,
+    );
   }
 
-  // TODO: add buildNameField()
+// TODO: add buildNameField()
 
 // TODO: add buildImportanceField()
 
