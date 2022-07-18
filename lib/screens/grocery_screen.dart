f21da2b9aa9172f253b472cfe65d8c443518fee2
tian <tianwu@outlook.com>
2022-07-18 17:16:23 +1000

    present placeholder grocery item screen

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../models/models.dart';
 import 'empty_grocery_screen.dart';
+import 'grocery_item_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({
@@ -18,22 +19,37 @@ class GroceryScreen extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          // TODO 11: present GroceryItemScreen
+          final groceryManager =
+              Provider.of<GroceryManager>(context, listen: false);
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => GroceryItemScreen(
+                onCreate: (item) {
+                  groceryManager.addItem(item);
+                  Navigator.pop(context);
+                },
+                onUpdate: (item) {},
+              ),
+            ),
+          );
+        },
       ),
       body: buildGroceryScreen(context),
     );
   }
+
   // TODO: add buildGroceryScreen
   Widget buildGroceryScreen(BuildContext context) {
-    return Consumer<GroceryManager>(
-        builder: (context, groceryManager, child) {
-          if (groceryManager.groceryItems.isNotEmpty) {
-            // TODO 25: add GroceryListScreen
-            return Container();
-          } else {
-            return EmptyGroceryScreen();
-          }
-        }
-    );
+    return Consumer<GroceryManager>(builder: (context, groceryManager, child) {
+      if (groceryManager.groceryItems.isNotEmpty) {
+        // TODO 25: add GroceryListScreen
+        return Container();
+      } else {
+        return EmptyGroceryScreen();
+      }
+    });
   }
 }
